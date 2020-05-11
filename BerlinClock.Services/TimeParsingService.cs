b355c80d1127542908c51ebc6b6f0fc8a4f6aa2c
Nick Reshetov <nick.reshetov@gmail.com>
2020-05-11 13:38:49 +0200

    Comminted ready solution.

---
+++ BerlinClock.Services/TimeParsingService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BerlinClock.Dtos;
+using BerlinClock.Services.Interfaces;
+
+namespace BerlinClock.Services
+{
+    public class TimeParsingService : ITimeParsingService
+    {
+        private const char FractionsSeparator = ':';
+
+        public TimeDto ParseTime(string time)
+        {
+            var timeFractions = time.Split(FractionsSeparator);
+
+            var timeDto = new TimeDto
+            {
+                Hours = ConvertTimeFractionToInt(timeFractions[0]),
+                Minutes = ConvertTimeFractionToInt(timeFractions[1]),
+                Seconds = ConvertTimeFractionToInt(timeFractions[2])
+            };
+
+            return timeDto;
+        }
+
+        private static int ConvertTimeFractionToInt(string timeFraction)
+        {
+            var timeFractionsSecondSymbol = timeFraction[1].ToString();
+            var isTimeFractionsFirstSymbolZero = timeFraction.StartsWith("0");
+
+            var normalizedTimeFraction = isTimeFractionsFirstSymbolZero ?
+                timeFractionsSecondSymbol :
+                timeFraction;
+
+            var convertedTimeFraction = Convert.ToInt32(normalizedTimeFraction);
+
+            return convertedTimeFraction;
+        }
+    }
+}
